ff3e8e0241c07d14f1b4a43b1f2c2fae414141c0
Samuel <you@example.com>
2023-01-16 04:45:11 +0100

    readme and style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,6 @@ import Card from "../components/Card";
 const Home = () => {
   return (
     <div className="home-container">
-      {/* <div className="home-container-img">
-        <h2 className="home-text-img">Chez vous, partout et ailleurs</h2>
-        <img
-          className="home-img"
-          src="./home_photo.jpg"
-          alt="paysages de côtes rocailleuse en face de la mer avec des cèdres et séquoia"
-        />
-      </div>
-      <div className="home-card-container">
-      </div> */}
       <div class="banner">
         <div class="banner-content">
           <h1>Chez vous, partout et ailleurs</h1>
@@ -25,4 +15,4 @@ const Home = () => {
 };
 
 export default Home;
-<h1>yo sur home</h1>;
+
