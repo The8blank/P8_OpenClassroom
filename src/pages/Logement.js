f148661a86525dce8459d252997d7303919f5046
samuel <samuelchojnacki.pro@gmail.com>
2023-01-17 10:48:13 +0100

    debut de la présentation de la page logement

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,6 +1,6 @@
 import { useParams,useNavigate } from "react-router-dom";
-import data from "../database/data";
 import { useEffect } from "react";
+import data from "../database/data";
 
 const Logement = () => {
   const { id } = useParams();
@@ -11,13 +11,52 @@ const Logement = () => {
     if (!card) {
       navigate("/404");
     }
+
+    
   }, [card, navigate]);
 
   if (card) {
     return (
         <>
-          <h3>{card.title}</h3>
-          <p>{card.description}</p>
+        {/* SLIDER */}
+          <div className="slider">
+            <h3>SLIDER</h3>
+          </div>
+
+          {/* Info Container */}
+          <div className="info-container">
+
+            {/* Info logement top titre, loc et tag host photo stars*/}
+            <div className="info-logement-top">
+              
+              {/* Info logement 1 titre, loc et tag */}
+              <div>
+                <h2 className="logement-title">{card.title}</h2>
+                <p className="logement-location">{card.location}</p>
+                <div className="tag-container">
+                  {card.tags.map(item => (
+                    <div>tag</div>
+                  ))}
+                </div>
+              </div>
+
+              {/* Info logement host name,photo et stars */}
+              <div className="info-host">
+                <h3 className="host-name">{card.host.name}</h3>
+                <div className="host-picture">photo</div>
+                <div className="host-rating">
+                  {card.rating}
+                </div>
+              </div>
+
+              
+            </div>
+
+            {/* info logement bot collapse */}
+            <div className="dropdown-container">
+              En attente encore 
+            </div>
+          </div>
         </>
     );
   }
